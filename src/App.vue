57d13214daec626b2d2fc024d1bf55ba185c4676
Thomas Chin <tchin25@gmail.com>
2021-06-04 12:14:17 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>{{ hasNFC() }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  methods: {
+    hasNFC() {
+      return "NDEFReader" in window
+    },
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
